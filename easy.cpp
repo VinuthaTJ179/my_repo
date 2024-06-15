a1f8574cb5fe97b04538baa8841174dbfb29b0d5
Vinutha TJ <vinuthatj16422@gmail.com>
2024-06-16 03:16:20 +0530

    EASY.CPP has been changed

--- easy.cpp
+++ easy.cpp
@@ -6,5 +6,12 @@ int main()
     cout << "This has to be changed" << endl;
     cout << "Nothing is impossible" << endl;
     cout << "You are the besttttt!!" << endl;
+    for (int i = 1; i <= 179; i++)
+    {
+        if (i == 179)
+        {
+            cout << "SUPER" << endl;
+        }
+    }
     return 0;
 }
